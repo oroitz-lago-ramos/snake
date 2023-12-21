1ff3fcb2b5cbb0cc48e03ab6e382fc856477953d
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-12-21 22:16:09 +0100

    Ajout textures snake

--- game/Snake.py
+++ game/Snake.py
@@ -13,7 +13,6 @@ class Snake:
         self.head_down = pg.image.load("graphics/assets/images/head_down.png")
         self.head_right = pg.image.load("graphics/assets/images/head_right.png")
         self.head_left = pg.image.load("graphics/assets/images/head_left.png")
-        
         self.head_up = pg.transform.scale(self.head_up, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         self.head_down = pg.transform.scale(self.head_down, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         self.head_right = pg.transform.scale(self.head_right, (graphics.CELL_SIZE, graphics.CELL_SIZE))
@@ -23,12 +22,24 @@ class Snake:
         self.tail_down = pg.image.load("graphics/assets/images/tail_down.png")
         self.tail_right = pg.image.load("graphics/assets/images/tail_right.png")
         self.tail_left = pg.image.load("graphics/assets/images/tail_left.png")
-        
         self.tail_up = pg.transform.scale(self.tail_up, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         self.tail_down = pg.transform.scale(self.tail_down, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         self.tail_right = pg.transform.scale(self.tail_right, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         self.tail_left = pg.transform.scale(self.tail_left, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         
+        self.body_vertical = pg.image.load("graphics/assets/images/body_vertical.png")
+        self.body_horizontal = pg.image.load("graphics/assets/images/body_horizontal.png")
+        self.body_vertical = pg.transform.scale(self.body_vertical, (graphics.CELL_SIZE, graphics.CELL_SIZE))
+        self.body_horizontal = pg.transform.scale(self.body_horizontal, (graphics.CELL_SIZE, graphics.CELL_SIZE))
+        
+        self.body_bl = pg.image.load("graphics/assets/images/body_bottomleft.png")
+        self.body_br = pg.image.load("graphics/assets/images/body_bottomright.png")
+        self.body_tl = pg.image.load("graphics/assets/images/body_topleft.png")
+        self.body_tr = pg.image.load("graphics/assets/images/body_topright.png")
+        self.body_bl = pg.transform.scale(self.body_bl, (graphics.CELL_SIZE, graphics.CELL_SIZE))
+        self.body_br = pg.transform.scale(self.body_br, (graphics.CELL_SIZE, graphics.CELL_SIZE))
+        self.body_tl = pg.transform.scale(self.body_tl, (graphics.CELL_SIZE, graphics.CELL_SIZE))
+        self.body_tr = pg.transform.scale(self.body_tr, (graphics.CELL_SIZE, graphics.CELL_SIZE))
         
         self.head = self.update_head_graphics()
         self.tail = self.update_tail_graphics()
@@ -49,7 +60,24 @@ class Snake:
                 elif index == len(self.body) - 1:
                     screen.blit(self.tail,body_rectangle)
                 else:
-                    pg.draw.rect(screen,'blue', body_rectangle)
+                    previous_block = self.body[index + 1] - block
+                    next_block = self.body[index - 1] - block
+                    if next_block.x == previous_block.x:
+                        screen.blit(self.body_vertical, body_rectangle)
+                    elif next_block.y == previous_block.y:
+                        screen.blit(self.body_horizontal, body_rectangle)
+                    else:
+                        if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                            screen.blit(self.body_tl,body_rectangle)
+                        elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                            screen.blit(self.body_bl,body_rectangle)
+                        elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                            screen.blit(self.body_tr,body_rectangle)
+                        elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                            screen.blit(self.body_br,body_rectangle)    
+                        
+                # else:
+                #     pg.draw.rect(screen,'blue', body_rectangle)
             else:
                     pg.draw.rect(screen,'blue', body_rectangle)
     
